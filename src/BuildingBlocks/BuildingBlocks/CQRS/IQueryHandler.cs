4ca67b2f4af184331e25445c2c9db81b2bd97a18
sujalramani84 <suzal84@outlook.com>
2024-08-13 18:42:15 +0530

    Developed CatalogAPI Endpoint, Handler and Infrastructure

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TQuery, TResonse>
+        : IRequestHandler<TQuery, TResonse>
+        where TQuery : IQuery<TResonse>
+        where TResonse : notnull
+    { 
+    
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler (IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Create Product entity from Command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            // Save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // return CreateProductResult result
+            return new CreateProductResult(product.Id);
+
+        }
+    }
+}
