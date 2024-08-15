59435b3280fee35ef12f4dd7e1a9e2aad4dd3609
sujalramani84 <suzal84@outlook.com>
2024-08-15 12:13:54 +0530

    Build Catalog Microservice with Vertical Slice Architecture and CQRS, also used Carter, Mapster, MediatR, Marten and FluentValidation library.

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -8,12 +8,11 @@ namespace Catalog.API.Products.GetProductByCategory
 
 
     internal class GetProductByCategoryQueryHandler
-        (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+        (IDocumentSession session)
         : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
     {
         public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
             var products = await session.Query<Product>()
                 .Where(p => p.Category.Contains(query.Category))
                 .ToListAsync(cancellationToken);
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -6,15 +6,14 @@ namespace Catalog.API.Products.GetProductById
     public record GetProductByIdResult(Product Product);
 
     internal class GetProductByIdQueryHandler 
-        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        (IDocumentSession session)
         : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>  
     {
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
             var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
             if (product is null) {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(query.Id);
             }
             return new GetProductByIdResult(product);
         }
