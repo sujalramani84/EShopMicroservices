adaba47e878a81e6ba0e4db597588f89e1fc15d5
sujalramani84 <suzal84@outlook.com>
2024-08-16 18:26:23 +0530

    Developed Basket Microservices with VSA and CQRS

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,34 @@
+
+using BuildingBlocks.Exceptions.Handler;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the containers.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddCarter();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+app.Run();
