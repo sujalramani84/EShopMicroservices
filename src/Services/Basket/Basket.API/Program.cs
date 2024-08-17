3a32e4bcbdba7352fac8f9e8cd90f5fb9f1478ee
sujalramani84 <suzal84@outlook.com>
2024-08-17 17:12:04 +0530

    add Redis caching into Basket microservices

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,5 @@
 
-using BuildingBlocks.Exceptions.Handler;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,8 +22,17 @@ builder.Services.AddMarten(opts =>
 builder.Services.AddCarter();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
@@ -31,4 +40,10 @@ var app = builder.Build();
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    }
+    );
 app.Run();
